c2d236596c79a638fede4ad970cfcb12bec0066f
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-05-12 10:53:52 +0200

    Dodano obsługę bazy SQLite i wykonano migrację| baza taksometr.db

---
+++ WpfProjektWirtualnyTaksometr/Migrations/20250512085152_InitialCreate.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfProjektWirtualnyTaksometr.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kierowcy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Imie = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefon = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    statusKierowcy = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kierowcy", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Klienci",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Imie = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefon = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Adres = table.Column<string>(type: "TEXT", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Klienci", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Taryfy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TypTaryfa = table.Column<string>(type: "TEXT", nullable: false),
+                    CenaZaKm = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Taryfy", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pojazdy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Marka = table.Column<string>(type: "TEXT", nullable: false),
+                    Model = table.Column<string>(type: "TEXT", nullable: false),
+                    NumerRejestracyjny = table.Column<string>(type: "TEXT", nullable: false),
+                    VIN = table.Column<string>(type: "TEXT", nullable: false),
+                    KierowcaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pojazdy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pojazdy_Kierowcy_KierowcaId",
+                        column: x => x.KierowcaId,
+                        principalTable: "Kierowcy",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Przejazdy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    KlientId = table.Column<int>(type: "INTEGER", nullable: false),
+                    KierowcaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PunktStartowy = table.Column<string>(type: "TEXT", nullable: false),
+                    PunktKoncowy = table.Column<string>(type: "TEXT", nullable: false),
+                    Kilometry = table.Column<double>(type: "REAL", nullable: false),
+                    Data = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TaryfaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cena = table.Column<decimal>(type: "TEXT", nullable: false),
+                    RodzajPlatnosci = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Przejazdy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Przejazdy_Kierowcy_KierowcaId",
+                        column: x => x.KierowcaId,
+                        principalTable: "Kierowcy",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Przejazdy_Klienci_KlientId",
+                        column: x => x.KlientId,
+                        principalTable: "Klienci",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Przejazdy_Taryfy_TaryfaId",
+                        column: x => x.TaryfaId,
+                        principalTable: "Taryfy",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pojazdy_KierowcaId",
+                table: "Pojazdy",
+                column: "KierowcaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Przejazdy_KierowcaId",
+                table: "Przejazdy",
+                column: "KierowcaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Przejazdy_KlientId",
+                table: "Przejazdy",
+                column: "KlientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Przejazdy_TaryfaId",
+                table: "Przejazdy",
+                column: "TaryfaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pojazdy");
+
+            migrationBuilder.DropTable(
+                name: "Przejazdy");
+
+            migrationBuilder.DropTable(
+                name: "Kierowcy");
+
+            migrationBuilder.DropTable(
+                name: "Klienci");
+
+            migrationBuilder.DropTable(
+                name: "Taryfy");
+        }
+    }
+}
